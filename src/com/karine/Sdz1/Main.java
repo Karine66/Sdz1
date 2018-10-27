33b40bef7515afe9f350a3b7eb7d9b885f145b29
Karine66 <kdbt1973>
2018-10-27 21:56:33 +0200

    Les classes, les getters, les setters, l'encapsulation, les constructeurs,

--- src/com/karine/Sdz1/Main.java
+++ src/com/karine/Sdz1/Main.java
@@ -3,13 +3,37 @@ package com.karine.Sdz1;
 public class Main {
 
     public static void main(String[] args) {
-	    Ville ville = new Ville();
-	    System.out.println(ville.nomVille);
-	    ville.nomVille = "la tête à toto !!!";
-	    System.out.println(ville.nomVille);
+        Ville v = new Ville();
+        Ville v1 = new Ville ("Marseille", 123456, "France");
+        Ville v2 = new Ville ("Rio", 321654,"Brésil");
 
-	    Ville ville2 = new Ville("Marseille", 123456789, "France");
-	    ville2.nomPays = "La tête à tutu !!!";
-	    System.out.println(ville2.nomPays);
+        System.out.println("\n v = "+v.getNom()+"ville de "+v.getNbreHabitants()+ "habitants se situant en "+v.getNomPays());
+        System.out.println(" v1 = "+v1.getNom()+" ville de "+v1.getNbreHabitants()+ " habitants se situant en"+v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de "+v2.getNbreHabitants()+ " habitants se situant en "+v2.getNomPays());
+
+        /*
+        Nous allons interchanger les villes v1 et v2
+        tout ça par l'intermédiaire d'un autre objet Ville.
+         */
+
+        Ville temp = new Ville();
+        temp = v1;
+        v1 = v2;
+        v2 = temp;
+
+        System.out.println("v1 = "+v1.getNom()+" ville de " +v1.getNbreHabitants()+ "habitants se situant en "+v1.getNomPays());
+        System.out.println("v2 = "+v2.getNom()+" ville de "+v2.getNbreHabitants()+ "habitants se situant en "+v2.getNomPays());
+
+        /*
+        Nous allons maintenant interchanger leurs noms
+        cette fois par le biais de leurs mutateurs
+         */
+
+        v1.setNom("Hong Kong");
+        v2.setNom("Djibouti");
+
+        System.out.println("v1 = "+v1.getNom()+" ville de "+v1.getNbreHabitants()+ " habitants se situant en "+v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de "+v2.getNbreHabitants()+ " habitants se sitant en "+v2.getNomPays());
+
     }
 }
--- src/com/karine/Sdz1/Ville.java
+++ src/com/karine/Sdz1/Ville.java
@@ -7,27 +7,117 @@ public class Ville {
     private String nomPays;
     /*Stocke le nombre d'habitants de notre ville*/
     private int nbreHabitants;
+    private char categorie;
 
     /*Constructeur par défaut*/
-    public Ville(){
+    Ville() {
         System.out.println("Création d'une ville !");
         nomVille = "Inconnu";
         nomPays = "Inconnu";
-        int nbreHabitants = 0;
+        nbreHabitants = 0;
+        this.setCategorie();
     }
 
     /*Constructeur avec paramètres*/
     /*J'ai ajouté un "p" en première lettre des paramètres*/
     /*Ce n'est pas une convention, mais ça peut être un bon moyen de les repérer*/
-    public Ville(String pNom, int pNbre, String pPays, int nbreHabitants)
-    {
+    Ville(String pNom, int pNbre, String pPays) {
         System.out.println("Création d'une ville avec des paramètres !");
         nomVille = pNom;
         nomPays = pPays;
         nbreHabitants = pNbre;
+        this.setCategorie();
+        nbreInstances++;
+        nbreInstancesBis++;
     }
 
-    public Ville(String marseille, int i, String france) {
+    /*Accesseurs (Getter)*/
+    /*Retourne le nom de la ville*/
+    public String getNom() {
+        return nomVille;
+    }
 
+    /*Retourne le nom du pays*/
+    public String getNomPays() {
+        return nomPays;
+    }
+
+    /*Retourne le nombre d'habitants*/
+    public int getNbreHabitants() {
+        return nbreHabitants;
+    }
+
+    /*Mutateurs (Setter)*/
+
+    /*Définit le nom de la ville*/
+    public void setNom(String pNom) {
+        nomVille = pNom;
+    }
+
+    /*Définit le nom du pays*/
+    public void setNomPays(String pPays) {
+        nomPays = pPays;
+    }
+
+    /*Définit le nombre d'habitants*/
+    public void setNombreHabitants(int nbre) {
+        nbreHabitants = nbre;
+        this.setCategorie();
+    }
+
+    /*
+Définit la catégorie de la ville
+ */
+    private void setCategorie() {
+
+    }
+
+    {
+
+        int bornesSuperieures[] = {0, 1000, 10000, 100000, 500000, 1000000, 5000000, 10000000};
+        char categories[] = {'?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
+        int i = 0;
+
+        this.categorie = categories[i];
+    }
+
+    /*
+    Retourne la description de la ville
+     */
+    public String decrisToi() {
+        return "\t" + this.nomVille + " est une ville de " + this.nomPays + " , elle comporte : " + this.nbreHabitants + " habitant(s) => elle est donc de catégorie : " + this.categorie;
+    }
+
+    /*Retourne une chaine de caractères selon le résultat de la comparaison
+     */
+    public String comparer(Ville v1) {
+        String str;
+
+        if (v1.getNbreHabitants() > this.nbreHabitants)
+            str = v1.getNom() + " est une ville plus peuplée que " + this.nomVille;
+
+        else str = this.nomVille + " est une ville plus peuplée que " + v1.getNom();
+
+        return str;
+    }
+
+    /*
+    Variable publiques qui comptent les instances
+     */
+    public static int nbreInstances = 0;
+
+    /*
+    Variable privée qui comptera aussi les instances
+     */
+    private static int nbreInstancesBis = 0;
+
+
+    public static int getNbreInstancesBis()
+    {
+        return nbreInstancesBis;
     }
 }
+
+
+
